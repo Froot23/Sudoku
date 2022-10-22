9379e21fc926ce707af97cc2fe9718af5d486cb4
garret <heatbag2014@gmail.com>
2022-10-22 14:00:11 -0700

    yoooooooo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 	    sodoko thing = new sodoko();
-        AtomicInteger jack = new AtomicInteger(1);
-        AtomicInteger bob = jack;
-        jack.set(4);
-        System.out.println(bob);
     }
 
 
--- src/com/company/sodoko.java
+++ src/com/company/sodoko.java
@@ -16,10 +16,9 @@ public class sodoko {
             for(int j =0; j<normal[i].length;j++)
                 normal[i][j] = new AtomicInteger();
         reference();
-        normal[5][5].set(9);
-        fill();
+        fill(0,0);
         print(normal);
-        print(special);
+        //print(special);
     }
     //TODO atomic int
     private void reference()
@@ -28,7 +27,7 @@ public class sodoko {
         {
             for(int j = 0; j < special[i].length; j++)
             {
-                special[i][j] = normal[j/3 + i/3][j%3 + i%3*3];
+                special[i][j] = normal[j/3 + i/3*3][j%3 + i%3*3];
             }
         }
     }
@@ -41,29 +40,62 @@ public class sodoko {
         }
     }
     //TODO maybe use intersection and union for speed
-    public void fill()
+    public boolean fill(int i, int j)
     {
+//        for(int i =0; i < normal.length;i++)
+//        {
+//            for(int j = 0; j < normal.length;j++)
+//            {
+//                HashSet choices = new HashSet(Arrays.asList(1,2,3,4,5,6,7,8,9));
+//                //HashSet row = new HashSet(Arrays.asList((normal[i])));
+//                //HashSet spe = new HashSet(Arrays.asList(special[i]));
+//                HashSet temp = new HashSet();
+//                for(AtomicInteger num: normal[i])
+//                    temp.add(num.get());
+//                choices.removeAll(temp);
+//                temp.clear();
+//                for(AtomicInteger num: special[j/3 + (i/3)*3])
+//                    temp.add(num.get());
+//                choices.removeAll(temp);
+//                temp.clear();
+//                for(int k = 0; k < normal.length;k++)
+//                    temp.add(normal[k][j].get());
+//                choices.removeAll(temp);
+//                int rand = (int) (Math.random() * choices.size());
+//                //TODO iterator rand
+//                normal[i][j].set((Integer)(choices.toArray()[rand]));
+//                System.out.println("\033[2J");
+//                print(normal);
+//                }
+//
+//            }
+        //TODO BACKTRACKING
         HashSet choices = new HashSet(Arrays.asList(1,2,3,4,5,6,7,8,9));
-        for(int i =0; i < normal.length;i++)
+        HashSet temp = new HashSet();
+        for(AtomicInteger num: normal[i])
+            temp.add(num.get());
+        choices.removeAll(temp);
+        temp.clear();
+        for(AtomicInteger num: special[j/3 + (i/3)*3])
+            temp.add(num.get());
+        choices.removeAll(temp);
+        temp.clear();
+        for(int k = 0; k < normal.length;k++)
+            temp.add(normal[k][j].get());
+        choices.removeAll(temp);
+        while(choices.size() != 0)
         {
-            for(int j = 0; j < normal.length;j++)
-            {
-                //HashSet row = new HashSet(Arrays.asList((normal[i])));
-                //HashSet spe = new HashSet(Arrays.asList(special[i]));
-                HashSet col = new HashSet();
-                for(int k = 0; i < normal.length;k++)
-                    col.add(normal[k][j]);
-                choices.removeAll(Arrays.asList((normal[i]));
-                choices.removeAll(Arrays.asList(special[i]));
-                choices.removeAll(col);
-                int rand = (int) (Math.random() * choices.size());
-                //TODO iterator rand
-                choices = new HashSet(Arrays.asList(1,2,3,4,5,6,7,8,9));
-                }
-
-            }
+            int rand = (int) (Math.random() * choices.size());
+            Integer num = (Integer)(choices.toArray()[rand]);
+            normal[i][j].set(num);
+            choices.remove(num);
+            if((i == 8 && j == 8) || fill(i+(j+1)/9, (j+1)%9))
+                return true;
         }
-    }
+        normal[i][j].set(0);
+        return false;
+        }
+
 
     private boolean checkRow(int num, int col)
     {
